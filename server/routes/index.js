4a9ab78380d98f0271e79cc6d81481ceddd4edf5
Valery Aligorsky <stdammit@gmail.com>
2018-01-19 13:45:29 +0300

    Added static middleware if flag exists.

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,20 @@
 'use strict';
 
-const express = require('express');
+const express = require('express')
+  , conf = require('../conf');
 
 const router = module.exports = new express.Router();
+
+if (process.env.SERVE_STATIC) {
+
+  router.all(/^(?!\/(api|public|i18n)).*$/, function (req, res, next) {
+    req.url = '/';
+    next();
+  });
+
+  router.use(express.static(conf.path('../client/dist')));
+
+}
 
 router.use(require('body-parser').json());
 
